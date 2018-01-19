521b224745c507f3b08d84e5116f62faa7b7250a
mridulafmach <mridula.prasad@fmach.it>
2018-01-19 13:10:24 +0100

    add description

---
+++ R/DHIimages.r
@@ -0,0 +1,11 @@
+#' Set of synthetic images for homogeneity assessment
+#'
+#' @name DHIimages
+#' @description This data set contains several synthetic images for drug homogeneity assessment problem.
+#' @usage data(DHIimages)
+#' @format DHIimages is a list of 20 images under four different scenarios, i.e., 5 basic images per scenario.
+#' \enumerate{
+#' \item In scenario 1, all images are with single gray level with different number of dark zones.
+#' \item In scenarios 2 and 3, similar images contain 2 and 4 gray levels, respectively.
+#' \item In scenario 4, all images are similar to the one in scenario 3 with added salt-pepper noise.
+#' }
